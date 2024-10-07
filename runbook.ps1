dac0821f7200f6ee799c0dae5359e1f5e07e0752
Alexander Appelby <94366402+Appelcloud@users.noreply.github.com>
2024-10-07 13:06:50 +0200

    Add files via upload

--- runbook.ps1
+++ runbook.ps1
@@ -1,21 +1,22 @@
-# Retrieve existing Automation Account variables
+# Retrieve automation account variables
 $AppSecret = Get-AutomationVariable -Name 'AppSecret'
 $ApplicationClientId = Get-AutomationVariable -Name 'ApplicationClientID'
 $TenantId = Get-AutomationVariable -Name 'TenantID'
+$MailUserId = Get-AutomationVariable -Name 'MailUserId'
 
-# Convert secret to a secure string and create the PSCredential
+# Convert client secret to secure string and create PSCredential object
 $SecureClientSecret = ConvertTo-SecureString -String $AppSecret -AsPlainText -Force
 $ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationClientId, $SecureClientSecret
 
-# Connect to Microsoft Graph using Client Secret Credential
+# Connect to Microsoft Graph
 Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential
 
 # Define mail recipient
 $MailRecipient = "aa@komplexit.dk"
 
-# Create output folder for the report
+# Create output folder
 $date = (Get-Date).ToString("yyyyMMdd-HHmm")
-$FileName = "MaesterReport" + $date + ".zip"
+$FileName = "MaesterReport" + $Date + ".zip"
 
 $TempOutputFolder = $env:TEMP + $date
 if (!(Test-Path $TempOutputFolder -PathType Container)) {
@@ -27,4 +28,4 @@ cd $env:TEMP
 md maester-tests
 cd maester-tests
 Install-MaesterTests .\tests
-Invoke-Maester -MailUserId "aa@3g35sp.onmicrosoft.com" -MailRecipient $MailRecipient -OutputFolder $TempOutputFolder
+Invoke-Maester -MailUserId $MailUserId -MailRecipient $MailRecipient -OutputFolder $TempOutputFolder
